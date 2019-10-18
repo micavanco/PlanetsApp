32c5ca8934112590002b9180f572e6319428af39
micavanco <micavanco@gmail.com>
2019-10-18 15:25:35 +0200

    Implemented logic to filter number  of page elements and changing pages

--- src/app/result-container/result-container.component.ts
+++ src/app/result-container/result-container.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IPlanet } from '../interfaces';
 
 @Component({
@@ -6,9 +6,30 @@ import { IPlanet } from '../interfaces';
   templateUrl: './result-container.component.html',
   styleUrls: ['./result-container.component.scss']
 })
-export class ResultContainerComponent {
+export class ResultContainerComponent implements OnInit {
   @Input() planets: Array<IPlanet>;
+  planetsModified: Array<IPlanet>;
+  indexFrom: number;
+  indexTo: number;
 
-  constructor() { }
+  constructor() {
+    this.indexFrom = 0;
+    this.indexTo = 10;
+  }
+
+  ngOnInit(): void {
+    this.planetsModified = this.planets.filter((e, i) => i >= this.indexFrom && i <= this.indexTo);
+  }
+
+  onPageChanged(e) {
+    const pageIndex = e.pageIndex;
+    const pageSize = e.pageSize;
+
+    this.indexFrom = pageIndex * pageSize;
+    this.indexTo = this.indexFrom + pageSize;
+
+    // tslint:disable-next-line:no-shadowed-variable
+    this.planetsModified = this.planets.filter((e, i) => i >= this.indexFrom && i < this.indexTo);
+  }
 
 }
